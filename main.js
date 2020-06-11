0dde4e041880ffd44c705c50d39fe067a1dd5396
Erick Walker <selfmade60@Ericks-MacBook-Pro.local>
2020-06-11 19:34:49 -0400

    changes made

--- main.js
+++ main.js
@@ -1,27 +1,58 @@
-// You should NOT change the HTML or CSS in this project (at least until you reach
-// the bonus objectives). Focus on the JavaScript.
+/* 
+
+User Experince:
+
+1. User looks and selects a row(rowsElement)
+2. User inputs some text found within that cells' innerHTML in the Find Input Box
+3. User inputs some value in the Replace With Input Box 
+4. The innerHTML of that cells element is changed to the users input value 
+
+*/
+
+
 
 const findInput = document.querySelector(".find-input")
 const replaceInput = document.querySelector(".replace-input")
 const replaceAllButton = document.querySelector(".replace-all-button")
 
-// The following variable holds your OUTER ARRAY of row elements.
-// Later you will need an OUTER LOOP to loop over the individual elements within
-// this array.
+
 const rowElements = document.querySelectorAll(".row")
 
-// When you call the function belwo, it will get and return an INNER ARRAY
-// containing the cell elements for a given row.
-// Call this function from WITHIN your row elements loop. Then you will, in turn,
-// need to loop over the resulting cell elements. But where should this whole
-// NESTED LOOP go? Think through the user's experience: when should WHAT happen? 
+
+
+
 function getCellElements (currentRowElement) {
     return currentRowElement.querySelectorAll(".cell")
 }
 
 
-// YOUR CODE GOES HERE
+replaceAllButton.addEventListener('click', function(){
 
+ 
+    for(let rowIndex = 0; rowIndex < rowElements.length; rowIndex ++){
+    
+        let currentRowElement = rowElements[rowIndex]
+    
+        //console.log(currentRowElement)
+    
+        let cellElements = getCellElements(currentRowElement);
+    
+        //console.log(currentElement)
+    
+        for(let cellElementIndex = 0; cellElementIndex < cellElements.length; cellElementIndex ++ ){
+    
+            let cellElement = cellElements[cellElementIndex]
+    
+            //console.log(cellElement)
+    
+            if(cellElement.innerHTML.includes(findInput.value)){
+        
+                cellElement.innerHTML = cellElement.innerHTML.replace(findInput.value, replaceInput.value)
+            }
+        }
+    }
+    //cellElement = newCellElement
+})
 
 // One last thing: dedicate very careful attention to using variables and
 // naming them accurately.
